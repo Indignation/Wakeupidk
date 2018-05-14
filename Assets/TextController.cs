257e62edf1c2c650c70a9778a676b07d0976e95b
LAPTOP-87JB9SST\Kim <kimpuolakka@gmail.com>
2018-05-14 15:27:35 +0300

    Small cleanup

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -10,20 +10,6 @@ public class TextController : MonoBehaviour {
 	public Text log;
 	public int wakeness = 0;
 
-	public void WakingUp()
-	{
-		print("test");
-		StartCoroutine("pause");
-	}
-
-//	IEnumerator pause()
-//	{
-//		log.text += "\n...";
-//		yield return new WaitForSecondsRealtime(3);
-//		print ("Coroutine finished");
-//		log.text += "\nfinished";
-//	}
-
 	IEnumerator wakeup()
 	{
 		for (int i = 0; i <= 10; i++)
@@ -34,24 +20,24 @@ public class TextController : MonoBehaviour {
 			yield return new WaitForSecondsRealtime(1);
 
 			int wakeuproll = Random.Range(1, 10);
-			if (wakeuproll <= 4)
-			{
+			if (wakeuproll <= 4) {
 				wakeness += 1;
 				log.text += "\nYour wakeness has improved by 1\n...";
 			}
-			else if (wakeness == 10)
-			{
+			
+			else if (wakeness == 10) {
 				log.text += "\nVICTORY!";
 				yield return new WaitForSecondsRealtime(1);
 				log.text += "\nYou slowly rise from your bed";
 				log.text += "\nPrepare for teeth brushing";
 			}
-			else
-			{
+			
+			else {
 				i--;
 			}
 		}
 	}
+	
 	void Start () {
 		log.text = "The game has started";
 		StartCoroutine("wakeup");
